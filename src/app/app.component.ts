4ebc49c397f6bc5306737f1de9c67a1d16295d7f
Celilsemi Sam Erkiner <celilsemi@erkiner.com>
2020-05-19 23:26:52 +1000

    remove menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,28 +18,6 @@ import { UserData } from './providers/user-data';
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
-  appPages = [
-    {
-      title: 'Schedule',
-      url: '/app/tabs/schedule',
-      icon: 'calendar'
-    },
-    {
-      title: 'Speakers',
-      url: '/app/tabs/speakers',
-      icon: 'people'
-    },
-    {
-      title: 'Map',
-      url: '/app/tabs/map',
-      icon: 'map'
-    },
-    {
-      title: 'About',
-      url: '/app/tabs/about',
-      icon: 'information-circle'
-    }
-  ];
   loggedIn = false;
   dark = false;
 
