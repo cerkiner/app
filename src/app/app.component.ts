473cc4e969c942183d0d05f9afda0affbcc0c36e
Celilsemi Sam Erkiner <celilsemi@erkiner.com>
2020-05-20 13:47:42 +1000

    menu design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,7 @@ import { UserData } from './providers/user-data';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   dark = true;
